b189d2608d428c4cd6501f965ce1e2670b7bc3b7
kbatson <kbatson@me.com>
2018-08-26 12:26:23 -0500

    Added night mode

--- html/js/kylebatson.js
+++ html/js/kylebatson.js
@@ -1,4 +1,68 @@
 $(document).ready(function(){
+	//** Night Mode **//
+	
+	var scrollOffset = 0;
+	var scrollColor = "";
+	
+	$("#nightToggle").on('click', function(){
+		if($("body").hasClass("night")){
+			$("body").removeClass("night");
+		} else {
+			$("body").addClass("night");
+		}
+	});
+	
+	$(window).scroll(function(){
+		if($('body').hasClass('night')){
+			scrollOffset = $(window).scrollTop() / ($('body').height() - $(window).height());
+			scrollColor = getColorForPercentage(scrollOffset);
+			$("#scrollPosition").html(scrollOffset).css("background-color", scrollColor);
+		
+			var colorTransition = function(link){
+				scrollOffset = ($(link).offset().top - $(window).scrollTop()) / $(window).height();
+				inverseScrollOffset = 1 - scrollOffset;
+				scrollColor = getColorForPercentage(scrollOffset);
+				inverseScrollColor = getColorForPercentage(inverseScrollOffset);
+				
+				if($(link).attr('id') == "resumeDownload"){
+					console.log('scrollOffset', scrollOffset, 'inverseScrollOffset', inverseScrollOffset);
+					$(link).css("background-color", scrollColor);
+				} else {
+					$(link).css("color", scrollColor);
+				}
+			}
+			
+			$('a').each(function(){
+				colorTransition($(this));
+			});
+		}
+	});
+
+    var percentColors = [
+    { pct: 0.0, color: { r: 0xf0, g: 0x95, b: 0x9a } },
+    { pct: 1.0, color: { r: 0x11, g: 0x3f, b: 68 } } ];
+
+	var getColorForPercentage = function(pct) {
+	    for (var i = 1; i < percentColors.length - 1; i++) {
+	        if (pct < percentColors[i].pct) {
+	            break;
+	        }
+	    }
+	    var lower = percentColors[i - 1];
+	    var upper = percentColors[i];
+	    var range = upper.pct - lower.pct;
+	    var rangePct = (pct - lower.pct) / range;
+	    var pctLower = 1 - rangePct;
+	    var pctUpper = rangePct;
+	    var color = {
+	        r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+	        g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+	        b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+	    };
+	    return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+	    // or output as hex if preferred
+	}
+	
 	//** Date **//
 	var thisYear = new Date().getFullYear();
 	$("#year").html(thisYear);
@@ -75,14 +139,6 @@ $(document).ready(function(){
 	$("#overlay").on('click', '.slide', function(){ //Navigate between slides by clicking on the slide itself
 		slideIndex = $(this).index();
 		nextSlide(slideIndex + 1);
-	});
-	
-	$(".slide").on('swipleft', function(){
-		switchSlide(slideIndex - 1);
-	});
-	
-	$(".slide").on('swiperight', function(){
-		switchSlide(slideIndex + 1);
 	});
 	
 	//********** Overlay **********//
