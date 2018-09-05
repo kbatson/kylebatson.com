ccebea1f6207e5506361fe9be1a4655ff7af19db
kbatson <kbatson@me.com>
2018-09-05 09:55:17 -0500

    Unified slideshow controls into a single file called from multiple pages

--- html/js/kylebatson.js
+++ html/js/kylebatson.js
@@ -71,75 +71,7 @@ $(document).ready(function(){
 	var sourceLink = "";
 	var portfolioURL = "";
 	var overlayContent = "";
-	
-	//** Slideshow **//
-	var numSlides = 0;
-	var slideIndex = "";
-	var slideWidth = 0;
-	var slideshowPosition = 0;
-	var resizeSlides = function(){
-		slideWidth = $("#overlay .slideshow").width();
-		$("#overlay .slideshow .slide").width(slideWidth);
-		setSlideshowDimensions();
-	}
-	var setSlideshowDimensions = function(){
-		numSlides = $("#overlay .slideshow .slide").length;
-		$("#overlay .slideshowContent").width(numSlides*slideWidth);
-	}
-	var createSlideshowControls = function(){
-		if(numSlides > 1){
-			for($i = 0; $i < numSlides; $i++){
-				if($i == 0){
-					$("#overlay .slideshowControls").append("<a href='#' class='slideControl active' data-slide='" + $i + "'>Slide " + 1 + "</a>");
-				} else {
-					$("#overlay .slideshowControls").append("<a href='#' class='slideControl' data-slide='" + $i + "'>Slide " + eval($i+1) + "</a>");
-				}
-			}
-		}
-	}
-	var removeSlideshowControls = function(){
-		$("#overlay .slideshowControls").html("");
-	}
-	
-	var selectSlide = function(element){ //Get proper index for any selected slide
-		slideIndex = $(element).attr("data-slide");
-		switchSlide(slideIndex);
-	}
-	
-	//** Switch Slides**//
-	var switchSlide = function(slideIndex){ //Switch to specified slide
-		slideshowPosition = eval(0 - $("#overlay .slide:eq(" + slideIndex + ")").position().left);
-		$("#overlay .slideshowContent").css("left", slideshowPosition);
-		$("#overlay .slideControl").each(function(){
-			$(this).removeClass("active");
-		});
-		$("#overlay .slideControl:eq(" + slideIndex + ")").addClass("active");
 
-	}
-	
-	var nextSlide = function(index){ //Return to beginning of slideshow if at the end
-		if(index < numSlides){
-			console.log('not last slide');
-			switchSlide(index);
-		} else {
-			console.log('last slide');
-			switchSlide(0);
-		}
-	}
-	
-	$(window).on('resize', function(){ //Ensure dimensions are updated if window is resized
-		resizeSlides();
-	});
-	
-	$("#overlay").on('click', '.slideControl', function(e){ //Navigate between slides by clicking on slide controls
-		selectSlide($(this));
-		return false;
-	});
-	
-	$("#overlay").on('click', '.slide', function(){ //Navigate between slides by clicking on the slide itself
-		slideIndex = $(this).index();
-		nextSlide(slideIndex + 1);
-	});
 	
 	//********** Overlay **********//
 	var numItems = $("#portfolio .sectionContent").find(".portfolioItem").length;
@@ -180,23 +112,6 @@ $(document).ready(function(){
 		$(sourceLink).focus(); //Return focus back to the element that spawned the overlay
 		$("body").removeClass("hasOverlay");
 	}
-
-/*
-	//Original version before AJAX call
-	var openOverlay = function(){
-		overlayContent = sourceLink.children(".portfolioItem-details").detach();
-		console.log('overlayContent', overlayContent);
-		$("#overlay").css("display", "grid").attr('aria-hidden', false).append(overlayContent);
-		$("header, main, footer").attr('aria-hidden', true);
-		$(".closeButton").focus();
-		$("body").addClass("hasOverlay");
-		generateOverlayControls();
-		
-		setSlideshowDimensions();
-		resizeSlides();
-		createSlideshowControls();
-	}
-*/
 	
 	$("#overlay").on('click', '.closeButton', function(){
 		closeOverlay();
@@ -210,17 +125,6 @@ $(document).ready(function(){
 			closeOverlay();
 		}
 	});
-	
-	//** Open Overlay **//
-/*
-	//Original version before AJAX call
-	$(".portfolioItem .portfolioLink").on('click', function(){
-		sourceLink = $(this).parent();
-		openOverlay();
-		
-		return false;
-	});
-*/
 
 	$(".portfolioLink a").on('click', function(){
 		sourceLink = $(this);
@@ -239,7 +143,6 @@ $(document).ready(function(){
 			$("body").addClass("hasOverlay");
 			generateOverlayControls();
 			
-			setSlideshowDimensions();
 			resizeSlides();
 			createSlideshowControls();
 		});
