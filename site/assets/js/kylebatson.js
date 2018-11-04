a54157219573311d67b863054c9e7bae72b19e12
kbatson <kbatson@me.com>
2018-11-04 10:52:32 -0600

    Used @supports to add support for IE

--- site/assets/js/kylebatson.js
+++ site/assets/js/kylebatson.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	//** Night Mode **//
 	
 	var scrollOffset = 0;
 	var scrollColor = "";
@@ -29,30 +28,30 @@ $(document).ready(function(){
 		// }
 	});
 
-    var percentColors = [
-    { pct: 0.0, color: { r: 0xf0, g: 0x95, b: 0x9a } },
-    { pct: 1.0, color: { r: 0x11, g: 0x3f, b: 68 } } ];
+ //    var percentColors = [
+ //    { pct: 0.0, color: { r: 0xf0, g: 0x95, b: 0x9a } },
+ //    { pct: 1.0, color: { r: 0x11, g: 0x3f, b: 68 } } ];
 
-	var getColorForPercentage = function(pct) {
-	    for (var i = 1; i < percentColors.length - 1; i++) {
-	        if (pct < percentColors[i].pct) {
-	            break;
-	        }
-	    }
-	    var lower = percentColors[i - 1];
-	    var upper = percentColors[i];
-	    var range = upper.pct - lower.pct;
-	    var rangePct = (pct - lower.pct) / range;
-	    var pctLower = 1 - rangePct;
-	    var pctUpper = rangePct;
-	    var color = {
-	        r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
-	        g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
-	        b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
-	    };
-	    return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
-	    // or output as hex if preferred
-	}
+	// var getColorForPercentage = function(pct) {
+	//     for (var i = 1; i < percentColors.length - 1; i++) {
+	//         if (pct < percentColors[i].pct) {
+	//             break;
+	//         }
+	//     }
+	//     var lower = percentColors[i - 1];
+	//     var upper = percentColors[i];
+	//     var range = upper.pct - lower.pct;
+	//     var rangePct = (pct - lower.pct) / range;
+	//     var pctLower = 1 - rangePct;
+	//     var pctUpper = rangePct;
+	//     var color = {
+	//         r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+	//         g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+	//         b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+	//     };
+	//     return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+	//     // or output as hex if preferred
+	// }
 	
 	//** Date **//
 	var thisYear = new Date().getFullYear();
